dda8f7d36e8434824baa5f603e78ffe1ff843070
Yashas Ip <67045923+yashasip@users.noreply.github.com>
2022-05-03 15:47:08 +0530

    Comments added

--- src/Constants.h
+++ src/Constants.h
@@ -2,15 +2,15 @@
 #define CONSTANTS_H
 
 #define PI 3.14159265358979323846
-#define RADIUS 30.0
-#define OFFSET 100
+#define RADIUS 30.0 // radius of circle token
+#define OFFSET 100 // the drawing of tokens starts from
 
 typedef enum color
 {
     NONE,
     RED,
     YELLOW
-}color;
+}color; // color palette
 
 #endif
 
--- src/Shapes.c
+++ src/Shapes.c
@@ -7,7 +7,8 @@ void draw_circle(int radius, int offset,color color_value)
     double y, x;
     glBegin(GL_POLYGON);
     {
-        glEnable(GL_POLYGON_SMOOTH);
+        glEnable(GL_POLYGON_SMOOTH); // enabling smooth drawing of polygon edges
+        /* set color of token */
         if (color_value == YELLOW)
             glColor3f(0.9216, 0.8784, 0);
         else if (color_value == RED)
@@ -15,11 +16,11 @@ void draw_circle(int radius, int offset,color color_value)
         else
             glColor4f(0, 0, 0, 0); // transparent color
 
-        for (double i = 0; i <= 360; i += 0.1)
+        for (double i = 0; i <= 360; i += 0.1) // draw circle based on polar equation
         {
             x = radius * cos(i * PI / 180);
             y = radius * sin(i * 3.142 / 180);
-            glVertex2d(offset + radius + x, offset + radius + y);
+            glVertex2d(offset + radius + x, offset + radius + y); // circle drawn at distance of offset + radius from window border
         }
     }
     glEnd();
