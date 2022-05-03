95b9d78c555b55664d3454c0e268486799e04ffc
Yashas Ip <67045923+yashasip@users.noreply.github.com>
2022-05-03 15:28:24 +0530

    Added Constants Header File
    includes all constants used in creation of shapes.

---
+++ src/Constants.h
@@ -0,0 +1,16 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define PI 3.14159265358979323846
+#define RADIUS 30.0
+#define OFFSET 100
+
+typedef enum color
+{
+    NONE,
+    RED,
+    YELLOW
+}color;
+
+#endif
+
