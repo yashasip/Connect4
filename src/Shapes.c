11f4f429f4b85f9ae72d869e2f7a3d2a434b7ad2
Yashas Ip <67045923+yashasip@users.noreply.github.com>
2022-05-07 12:58:36 +0530

    Fixed wrong value of whit color

--- src/Shapes.c
+++ src/Shapes.c
@@ -1,5 +1,6 @@
 #include <GL/glut.h>
 #include<math.h>
+#include<stdio.h>
 #include "Constants.h"
 
 void draw_circle(int radius, int offset,color color_value)
@@ -14,7 +15,7 @@ void draw_circle(int radius, int offset,color color_value)
         else if (color_value == RED)
             glColor3f(0.9216, 0.1961, 0.3608);
         else
-            glColor4f(0, 0, 0, 0); // transparent color
+            glColor4f(1, 1, 1, 1); // transparent color
 
         for (double i = 0; i <= 360; i += 0.1) // draw circle based on polar equation
         {
@@ -26,3 +27,15 @@ void draw_circle(int radius, int offset,color color_value)
     glEnd();
 }
 
+void make_haunch(int radius, int offset){
+    glBegin(GL_QUADS);
+    printf("hello");
+    {
+        glColor3f(0.3255, 0.2235, 0.9961);
+        glVertex2i(100, 10);
+        glVertex2i(600, 10);
+        glVertex2i(600, 600);
+        glVertex2i(100, 600);
+    }
+    glEnd();
+}
