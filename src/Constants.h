7252dbbd7baa4d5121c9c0c8a575837361421a90
Yashas Ip <67045923+yashasip@users.noreply.github.com>
2022-06-12 17:56:23 +0530

    Added new Constants and Bool enum

--- src/Constants.h
+++ src/Constants.h
@@ -2,13 +2,15 @@
 #define CONSTANTS_H
 
 #define PI 3.14159265358979323846
-#define RADIUS 30.0 // radius of circle token
+#define RADIUS 30.0  // radius of circle token
 #define OFFSET_X 100 // the drawing of objects starts from offset
-#define OFFSET_Y 10 
+#define OFFSET_Y 10
 #define WINDOW_WIDTH 820.0
 #define WINDOW_HEIGHT 660.0
 #define BOARD_WIDTH 620
 #define BOARD_HEIGHT 535
+#define BOARD_COLUMN_COUNT 7
+#define BOARD_ROW_COUNT 6
 #define SLOT_MARGIN 25
 
 typedef enum color
@@ -16,7 +18,12 @@ typedef enum color
     NONE,
     RED,
     YELLOW
-}color; // color palette
+} color; // color palette
+
+typedef enum bool
+{
+    false,
+    true
+} bool; // user-defined boolean
 
 #endif
-
