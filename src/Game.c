d41d1c92229a5b255b0905ea3846b97c0e8e36b8
Yashas Ip <67045923+yashasip@users.noreply.github.com>
2022-06-12 10:47:35 +0530

    Added Keyboard Interaction control

--- src/Game.c
+++ src/Game.c
@@ -2,9 +2,13 @@
 #include "Constants.h"
 #include "Shapes.h"
 
+int placeTokenX = OFFSET_X + SLOT_MARGIN;
+int placeTokenY = OFFSET_Y + BOARD_HEIGHT + RADIUS;
+
 void init();
 void startGame();
 void drawSlots();
+void keyboardInput(int, int, int);
 
 void Connect4(int argc, char *argv[])
 {
@@ -14,6 +18,7 @@ void Connect4(int argc, char *argv[])
     glutInitWindowPosition(glutGet(GLUT_SCREEN_HEIGHT) / 2, 0);
     glutCreateWindow("Connect 4");
     init(); // initialize parameters
+    glutSpecialFunc(keyboardInput);
     glutDisplayFunc(startGame);
     glutMainLoop();
 }
@@ -29,6 +34,27 @@ void drawSlots()
     for (int j = OFFSET_Y + SLOT_MARGIN, row = 0; row < 6; j += 2 * RADIUS + SLOT_MARGIN, row++)
         for (int i = OFFSET_X + SLOT_MARGIN, column = 0; column < 7; i += 2 * RADIUS + SLOT_MARGIN, column++)
             drawCircle(RADIUS, i, j, NONE);
+    drawCircle(RADIUS, placeTokenX, placeTokenY, YELLOW); // place token
+}
+
+void keyboardInput(int key, int x, int y){
+
+    switch(key){
+        case GLUT_KEY_RIGHT:
+            placeTokenX = placeTokenX + 2 * RADIUS + SLOT_MARGIN;
+            if (placeTokenX > OFFSET_X + BOARD_WIDTH - SLOT_MARGIN)
+                placeTokenX = OFFSET_X + SLOT_MARGIN;
+            break;
+        case GLUT_KEY_LEFT:
+            placeTokenX = placeTokenX - 2 * RADIUS - SLOT_MARGIN;
+            if (placeTokenX < OFFSET_X + SLOT_MARGIN)
+                placeTokenX = OFFSET_X + BOARD_WIDTH - SLOT_MARGIN - 2 * RADIUS;
+            break;
+        default:
+            return;
+    }
+
+    glutPostRedisplay();
 }
 
 void startGame()
@@ -36,6 +62,6 @@ void startGame()
     glClear(GL_COLOR_BUFFER_BIT);
     drawBoard();
     drawSlots();
-
+    
     glFlush();
 }
